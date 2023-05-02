3f2c082e4389ff1a77b25923b55d93eb199eb214
JSR5404 <joshrice5404@gmail.com>
2023-05-01 23:25:49 -0400

    mongo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jsr5404:8JvbczGYJFXEBbX1@atlascluster.a80m33j.mongodb.net/?retryWrites=true&w=majority');
 
 module.exports = mongoose.connection;
